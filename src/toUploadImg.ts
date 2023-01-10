28eaf7fa6b78f1287122e1c121334ead70af21be
dafengxiang <1205679862@qq.com>
2023-01-10 19:27:48 +0800

    feat: 安装熊猫依赖

--- src/toUploadImg.ts
+++ src/toUploadImg.ts
@@ -2,11 +2,13 @@
  * @Description:
  * @Author: wangfengxiang
  * @Date: 2023-01-10 15:40:05
- * @LastEditTime: 2023-01-10 16:15:06
+ * @LastEditTime: 2023-01-10 19:23:10
  * @LastEditors: wangfengxiang
  */
 import * as vscode from "vscode";
-const toUploadImg = vscode.commands.registerCommand("uploadimg", async () => {
+import * as fs from 'fs'
+
+const toUploadImg = vscode.commands.registerCommand("uploadimg", async () => {console.log(222);
     const uri = await vscode.window.showOpenDialog({
         canSelectFolders: false,
         canSelectMany: false,
@@ -14,8 +16,9 @@ const toUploadImg = vscode.commands.registerCommand("uploadimg", async () => {
           images: ['png', 'jpg','apng','jpeg','gif','webp'],
         },
       }); 
-
-      console.log('uri: ', uri);
-  vscode.window.showInformationMessage("代码生成成功！");
+      //@ts-ignore
+      let imgBuffer =  fs.readFileSync(uri[0].path);
+      console.log('uri:111999999999 ');
+  vscode.window.showInformationMessage("代码生成成功！",imgBuffer);
 });
 export default toUploadImg;
